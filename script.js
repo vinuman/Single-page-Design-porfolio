37391a6c6245d99e320e18c0cd7a5bcac3cd8ce1
vinu cyril <38722628+vinuman@users.noreply.github.com>
2023-03-29 19:05:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-
-
-const header = document.querySelector('header');
-const main = document.querySelector('main');
 const leftButton = document.querySelector('.left-button');
 const rightButton = document.querySelector('.right-button');
 const imageContainer = document.querySelector('.image-container');
+const myWork = document.querySelectorAll('.image-container img');
 let activeButton;
 
-rightButton.addEventListener('click', ()=>{
-    if(activeButton == leftButton){
-        imageContainer.classList.remove('right');
-        imageContainer.classList.remove('left');
-        activeButton = null;
-    }else{
-        imageContainer.classList.add('right');
-        imageContainer.classList.remove('left');
-        activeButton = rightButton;
-    }
-   
-    
-});
+let counter = 0;
+const size = window.innerWidth < 640? 300 : 540;
 
-leftButton.addEventListener('click', ()=>{
-    if(activeButton == rightButton){
-        imageContainer.classList.remove('right');
-        imageContainer.classList.remove('left');
-        activeButton = null;
-    } else{
-        imageContainer.classList.add('left');
-        imageContainer.classList.remove('right');
-        activeButton = leftButton;
+
+imageContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+rightButton.addEventListener('click', () => {
+    console.log(size);
+    if (counter >= myWork.length - 1) return;
+    imageContainer.style.transition = "transform 0.5s ease-in-out";
+    counter++;
+    imageContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    leftButton.disabled = false;
+    if (counter === myWork.length - 1) {
+        rightButton.disabled = true;
     }
-    
-});
+  });
+  
+  leftButton.addEventListener('click', () => {
+    if (counter <= 0) return;
+    imageContainer.style.transition = "transform 0.5s ease-in-out";
+    counter--;
+    imageContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    rightButton.disabled = false;
+    if (counter === 0) {
+      leftButton.disabled = true;
+    }
+  });
+
+
+
+
